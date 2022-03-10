019d8b779138190dbbd93e3fbe7c682592807a63
dposza <davidposza@outlook.com>
2022-03-10 14:27:52 +0100

    great

--- hmmm.py
+++ hmmm.py
@@ -4,5 +4,7 @@ import pandas as pd
 con = sqlite3.connect('Portfolios.db')
 cur = con.cursor()
 
-df= pd.read_sql_query("SELECT * from Portfolio1", con)
+df = pd.read_sql_query("SELECT * from Portfolio1", con)
 print(df)
+
+print('yay it works')
